f19973f4b5e6aa67fe4f216d73c4d444e13595ad
JNInRain <3291433379@qq.com>
2024-05-21 14:30:15 +0800

    创建

---
+++ src/main/java/com/jninrain/sunoai/SunoAiApplication.java
@@ -0,0 +1,13 @@
+package com.jninrain.sunoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunoAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunoAiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jninrain/sunoai/util/Result/SunoApiUtil.java
@@ -0,0 +1,17 @@
+package com.jninrain.sunoai.util.Result;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 封装第三方SunoApi生成音乐接口
+ *
+ * @Auther: fei
+ * @Date: 2024/05/21/14:03
+ * @Description:
+ */
+@Slf4j
+public class SunoApiUtil {
+    public static  final  String host = "";
+
+
+}
