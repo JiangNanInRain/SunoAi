725660336b158cf35c6c8272fa168d72d8291680
JNInRain <3291433379@qq.com>
2024-05-27 17:46:52 +0800

    添加跨域配置

---
+++ src/main/java/com/jninrain/sunoai/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.jninrain.sunoai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: fei
+ * @Date: 2024/05/27/09:05
+ * @Description:
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
