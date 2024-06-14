eb96ed9a2e0c2413e25de1fa7c0425dd9acfc315
JNInRain <3291433379@qq.com>
2024-06-14 19:03:43 +0800

    做热度榜

--- src/test/java/com/jninrain/sunoai/SunoAiApplicationTests.java
+++ src/test/java/com/jninrain/sunoai/SunoAiApplicationTests.java
@@ -1,21 +1,28 @@
 package com.jninrain.sunoai;
 
 import com.jninrain.sunoai.util.Redis.JedisPoolUtil;
+import com.jninrain.sunoai.util.Redis.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class SunoAiApplicationTests {
 
-
+    @Resource
+    private  RedisUtil redisUtil;
     JedisPool jedisPool = JedisPoolUtil.getJedisPoolInstance();
 
 
 
     @Test
     public void test2() {
+        redisUtil.set("名字","王一哲");
+        System.out.println(redisUtil.get("名字"));
+        String[] songIdList;
 
     }
 
